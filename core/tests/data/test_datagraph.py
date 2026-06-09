42fd91f0c4a3a8c0476aab2284ae836859c65676
Erik Small <erik.small@ensc.dev>
2026-06-08 20:50:55 -0600

    Simplify DataGraph test op

--- core/tests/data/test_datagraph.py
+++ core/tests/data/test_datagraph.py
@@ -45,33 +45,26 @@ class TestOp(DataOp):
     out_kind: str = "buffer"
 
     async def process(self, incoming: list[DataFlow], outgoing: list[DataFlow]):
-        if self.in_kind == "buffer":
-            context, buffer = await incoming[0].receive("buffer")
-            reader = None
-        else:
-            context, reader = await incoming[0].receive("stream")
-            buffer = None
-
-        assert reader is not None or buffer is not None
+        context, data = await incoming[0].receive(self.in_kind)
         context["count"] += 1
 
         if self.out_kind == "buffer":
-            if buffer is not None:
-                await outgoing[0].send(context, buffer)
+            if self.in_kind == "buffer":
+                await outgoing[0].send(context, data)
             else:
                 combined = bytearray()
 
-                async for chunk in reader:
+                async for chunk in data:
                     combined.extend(chunk)
 
                 await outgoing[0].send(context, combined)
         else:
             writer = await outgoing[0].send(context)
 
-            if buffer is not None:
-                writer.write(buffer)
+            if self.in_kind == "buffer":
+                writer.write(data)
             else:
-                async for chunk in reader:
+                async for chunk in data:
                     writer.write(chunk)
 
             writer.close()
